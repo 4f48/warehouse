962e76f59664a32ac5d4a2d05652ba15212dc4ef
4f48 <0x4f48@proton.me>
2024-12-27 20:49:55 +0100

    error handling, check for file name

--- src/routes/upload.rs
+++ src/routes/upload.rs
@@ -1,11 +1,26 @@
-use axum::extract::Multipart;
+use axum::{extract::Multipart, http::StatusCode};
 
-pub(crate) async fn upload(mut multipart: Multipart) {
-    // parse multipart using serde
-    
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let bytes = field.bytes().await.unwrap();
+pub(crate) async fn upload(mut multipart: Multipart) -> Result<StatusCode, StatusCode> {
+    while let Some(field) = match multipart.next_field().await {
+        Ok(field) => field,
+        Err(_) => return Err(StatusCode::BAD_REQUEST),
+    } {
+        let name = match field.name() {
+            Some(name) => name,
+            None => return Err(StatusCode::BAD_REQUEST),
+        };
+
+        if name != "file" {
+            return Err(StatusCode::BAD_REQUEST);
+        }
+
+        let bytes = match field.bytes().await {
+            Ok(bytes) => bytes,
+            Err(_) => return Err(StatusCode::BAD_REQUEST),
+        };
+
         println!("{:?}", bytes);
     }
-    
+
+    Ok(StatusCode::OK)
 }
